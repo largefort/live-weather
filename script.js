8cc5de3566100bb5d9d28dbd6f6ccaef6fcc2570
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-02-13 17:36:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-// Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
-const apiKey = 'd0cb3d09f67d9502d6b77713d580d5fa';
+// Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual OpenWeatherMap API key
+const openWeatherMapApiKey = 'd0cb3d09f67d9502d6b77713d580d5fa';
+
+// Replace 'YOUR_GOOGLE_MAPS_API_KEY' with your actual Google Maps API key
+const googleMapsApiKey = 'AIzaSyBmjZTl720lA-j4i2UgRUv2pm0Q48Ai5mc';
+
 const mapElement = document.getElementById('map');
 
-// Function to initialize the map
 function initMap() {
-    // Set the initial map coordinates
     const map = new google.maps.Map(mapElement, {
         center: { lat: 0, lng: 0 },
         zoom: 2
     });
 
-    // Request user's location (optional)
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const userLocation = {
@@ -19,8 +20,6 @@ function initMap() {
             };
             map.setCenter(userLocation);
             map.setZoom(8);
-
-            // Call the function to fetch and display weather data
             fetchWeatherData(userLocation.lat, userLocation.lng);
         }, handleError);
     } else {
@@ -28,18 +27,15 @@ function initMap() {
     }
 }
 
-// Function to fetch and display weather data
 function fetchWeatherData(latitude, longitude) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherMapApiKey}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Extract relevant weather information from the API response
             const temperature = data.main.temp;
             const cityName = data.name;
 
-            // Display the weather information on the map
             const infowindow = new google.maps.InfoWindow({
                 content: `City: ${cityName}<br>Temperature: ${temperature}°C`
             });
@@ -59,19 +55,16 @@ function fetchWeatherData(latitude, longitude) {
         });
 }
 
-// Handle geolocation errors
 function handleError(error) {
     console.error('Geolocation error:', error.message);
 }
 
-// Load the Google Maps API and initialize the map
 function loadMapScript() {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&callback=initMap`;
     script.defer = true;
     script.async = true;
     document.head.appendChild(script);
 }
 
-// Load the map script when the page is loaded
 window.onload = loadMapScript;
